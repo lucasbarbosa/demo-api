c0dba63da8bb704d77ef3ee0affea58f59fc8033
Lucas Barbosa <lucasbtsilva@gmail.com>
2026-02-02 23:11:17 -0300

    feat: update swagger

--- core/swagger-jwt-docker/src/DemoApi.Api/Configuration/SwaggerConfig.cs
+++ core/swagger-jwt-docker/src/DemoApi.Api/Configuration/SwaggerConfig.cs
@@ -1,4 +1,4 @@
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
 
 namespace DemoApi.Api.Configuration;
 
@@ -37,18 +37,11 @@ public static class SwaggerConfig
                 BearerFormat = "JWT"
             });
 
-            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            c.AddSecurityRequirement(doc => new OpenApiSecurityRequirement
             {
                 {
-                    new OpenApiSecurityScheme
-                    {
-                        Reference = new OpenApiReference
-                        {
-                            Type = ReferenceType.SecurityScheme,
-                            Id = "Bearer"
-                        }
-                    },
-                    Array.Empty<string>()
+                    new OpenApiSecuritySchemeReference("Bearer", doc),
+                    new List<string>()
                 }
             });
         });
